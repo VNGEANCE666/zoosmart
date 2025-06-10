072b23b5a545246c70c10d9b52c0f20b570ae821
rizwaradnan03 <rizwarbyan123@gmail.com>
2025-06-10 17:04:06 +0700

    .

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main_layout')
 @section('content')
-    <main style="padding: 2rem;">
-    <h1>About Us</h1>
-    <p>Zoo Smart is committed to animal conservation, research, and educational outreach to inspire the next generation of environmental stewards.</p>
-  </main>
+  <section class="page-content">
+    <h1>About Zoosmart</h1>
+    <p>Zoosmart is a modern zoo experience platform...</p>
+  </section>
 @endsection
--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="{{asset('main.css')}}">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>@yield('title', 'Zoosmart')</title>
+  <link rel="stylesheet" href="{{ asset('main.css') }}" />
 </head>
-
 <body>
+  <header>
+    <div class="logo">Zoo<span>SMART</span></div>
     <nav>
-        <div class="logo">Zoo<span>SMART</span></div>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="{{route('activities')}}">Activities</a></li>
-            <li><a href="{{route('about')}}">About Us</a></li>
-            <li><a href="{{route('contact')}}">Contact Us</a></li>
-            <li><a href="{{route('animal')}}">Animal & Zones</a></li>
-        </ul>
-        <a href="login.html" class="login-btn">Login</a>
+      <ul>
+        <li><a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
+        <li><a href="{{ route('activities') }}" class="{{ Request::is('activities') ? 'active' : '' }}">Activities</a></li>
+        <li><a href="{{ route('about') }}" class="{{ Request::is('about') ? 'active' : '' }}">About us</a></li>
+        <li><a href="{{ route('contact') }}" class="{{ Request::is('contact') ? 'active' : '' }}">Contact Us</a></li>
+        <li><a href="{{ route('animal') }}" class="{{ Request::is('animal') ? 'active' : '' }}">Animal & Zones</a></li>
+      </ul>
     </nav>
+    <a href="{{ route('auth.login-page') }}" class="login-btn">Login</a>
+  </header>
 
+  <main>
     @yield('content')
+  </main>
 
-    <footer>
-        © 2025 - {{env('APP_NAME')}}. All rights reserved. Address Address
-    </footer>
-
+  <footer>
+    © 2025 - Zoosmart. All rights reserved. Address Address
+  </footer>
 </body>
-
 </html>
